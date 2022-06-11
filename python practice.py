aba25fefe4c3dc74a89c2bbb3eb0d288dcb3d65c
UdayGhorapade <95113723+UdayGhorapade@users.noreply.github.com>
2022-06-11 20:14:48 +0530

    Add files via upload

---
+++ python practice.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x='uday'
+y='Ghorapade'
+print(x)
+print(y)
+
+
+# In[2]:
+
+
+print(x,y)
+
+
+# In[4]:
+
+
+print(x,end='-')
+print(y)
+
+
+# In[10]:
+
+
+name=input('enter your name : ')
+print('hello ' + name)
+
+
+# In[11]:
+
+
+z=x+y
+print(z)
+
+
+# In[12]:
+
+
+a = "hello world"
+a[0:5:1]
+
+
+# In[13]:
+
+
+first_name = 'uday '
+last_name = 'Ghorapade'
+full_name = first_name+last_name
+print(full_name)
+
+
+# In[14]:
+
+
+a='uday'
+print(a.upper())
+
+
+# In[15]:
+
+
+a='UDAY'
+print(a.lower())
+
+
+# In[ ]:
+
+
+
+
