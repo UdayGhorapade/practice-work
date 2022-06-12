02016046f7faa8a437694099077ebc9b64512eb9
UdayGhorapade <95113723+UdayGhorapade@users.noreply.github.com>
2022-06-12 22:38:24 +0530

    Add files via upload

---
+++ python practice (list).py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list1 = [1,2,3,4.5,5.5,"maths","python",True,False]
+list1
+
+
+# In[2]:
+
+
+list2=['uday','ghorapade']
+list2
+
+
+# In[3]:
+
+
+list1[0]='mr'
+list1
+
+
+# In[4]:
+
+
+list1.append(list2)
+print(list1)
+
+
+# In[7]:
+
+
+list1.insert(2,'manuchiwadi')
+list1
+
+
+# In[8]:
+
+
+list1.extend(list2)
+
+
+# In[9]:
+
+
+list1
+
+
+# In[10]:
+
+
+list1.pop(2)
+list1
+
+
+# In[ ]:
+
+
+pyrthon 
+
