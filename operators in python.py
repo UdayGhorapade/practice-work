0302a2778d58b0045561fc1a52f25d5b85c6f771
UdayGhorapade <95113723+UdayGhorapade@users.noreply.github.com>
2022-06-15 20:46:01 +0530

    Add files via upload

---
+++ operators in python.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#We will discuss about some of the important operators in python
+#Arithmatic Operators
+#Assignment Operators
+#Comparision Operators
+#Logical Operators
+
+
+# In[1]:
+
+
+#arithmetic operators 
+x=10
+y=20
+z=x+y
+z
+
+
+# In[2]:
+
+
+x-y
+
+
+# In[3]:
+
+
+x/y
+
+
+# In[4]:
+
+
+x*y
+
+
+# In[7]:
+
+
+#2) Assignment Operators
+x=10
+x=x+5
+x
+
+
+# In[8]:
+
+
+#3) Comparision Operators 
+x = 5
+y = 2
+x == y
+
+
+# In[9]:
+
+
+x != y
+
+
+# In[10]:
+
+
+x > y
+
+
+# In[11]:
+
+
+#4) Logical operators ( or , and , not )
+x = True
+y = False
+x and y
+
+
+# In[12]:
+
+
+x or y
+
+
+# In[13]:
+
+
+not x
+
+
+# In[ ]:
+
+
+
+
