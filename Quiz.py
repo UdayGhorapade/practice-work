8bab83bb8de9fbe1d334ee909b49597fe647b165
UdayGhorapade <95113723+UdayGhorapade@users.noreply.github.com>
2022-07-14 20:20:06 +0530

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+set1={"uday",2,"Sam",3}
+
+set1.remove(3)
+print(set1)
+
+
+# In[2]:
+
+
+sampleSet = {"Yellow", "Orange", "Black"}
+
+sampleSet.add("Blue")
+
+sampleSet.add("Orange")
+
+print(sampleSet)
+
+
+# In[6]:
+
+
+A = {0, 2, 4, 6, 8}; 
+
+B = {1, 2, 3, 4, 5}; 
+
+print(A & B)
+
+
+# In[ ]:
+
+
+
+
