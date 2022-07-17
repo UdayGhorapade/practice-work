b9ba7b8ace25971ae4ffe3dafc901308d44d09a7
UdayGhorapade <95113723+UdayGhorapade@users.noreply.github.com>
2022-07-17 20:17:14 +0530

    Add files via upload

---
+++ python quiz.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#.append is used to add elements in the list
+lst=['Mathematics', 'chemistry', 100, 200, 300, 204]
+lst.append("Krish")
+lst
+
+
+# In[5]:
+
+
+lst.append(["John","Bala"])
+lst
+
+
+# In[6]:
+
+
+lst.insert(2,"Naik")
+lst
+
+
+# In[7]:
+
+
+lst=[1,2,3]
+lst.extend([8,9])
+lst
+
+
+# In[9]:
+
+
+
+lst=[1,2,3,4,5]
+sum(lst)
+
+
+# In[10]:
+
+
+lst*5
+
+
+# In[11]:
+
+
+lst.pop()
+
+
+# In[12]:
+
+
+lst=[1,1,2,3,4,5]
+lst.count(1)
+
+
+# In[13]:
+
+
+lst.index(1,1,4)
+
+
+# In[14]:
+
+
+set_var= set()
+print(set_var)
+print(type(set_var))
+
+
+# In[ ]:
+
+
+
+
