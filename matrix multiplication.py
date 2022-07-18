0d3ee9b9fda819971d6ba5118a71a70020340816
UdayGhorapade <95113723+UdayGhorapade@users.noreply.github.com>
2022-07-18 20:54:57 +0530

    Add files via upload

---
+++ matrix multiplication.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+x = np.random.random((5,3))
+print("First array:")
+print(x)
+
+
+# In[3]:
+
+
+y = np.random.random((3,2))
+print("Second array:")
+print(y)
+
+
+# In[4]:
+
+
+z = np.dot(x, y)
+print("Dot product of two arrays:")
+print(z)
+
+
+# In[ ]:
+
+
+
+
